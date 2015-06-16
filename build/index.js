3beb93c93a6cbd739eaa2349d46e6d34f5c23a08
David Eisinger <david.eisinger@gmail.com>
2015-06-16 10:52:57 -0400

    Two-space indents

--- build/index.js
+++ build/index.js
@@ -7,90 +7,90 @@
 //
 
 var items = [
-    {
-        title: "Thing #1",
-        completed: false
-    },
-    {
-        title: "Thing #2",
-        completed: true
-    },
-    {
-        title: "Thing #3",
-        completed: false
-    }
+  {
+    title: "Thing #1",
+    completed: false
+  },
+  {
+    title: "Thing #2",
+    completed: true
+  },
+  {
+    title: "Thing #3",
+    completed: false
+  }
 ];
 
 var TodoItem = React.createClass({displayName: "TodoItem",
 
-    getDefaultProps: function() {
-        return {
-            title     : '',
-            completed : false
-        };
-    },
+  getDefaultProps: function() {
+    return {
+      title   : '',
+      completed : false
+    };
+  },
 
-    getInitialState: function() {
-        return {
-            completed : this.props.completed,
-            editing   : false
-        };
-    },
+  getInitialState: function() {
+    return {
+      completed : this.props.completed,
+      editing   : false
+    };
+  },
 
-    toggleCompleted: function(v) {
-        this.setState({ completed: !this.state.completed });
-    },
+  toggleCompleted: function(v) {
+    this.setState({ completed: !this.state.completed });
+  },
 
-    toggleEditing: function() {
-        this.setState({ editing: true });
-    },
+  toggleEditing: function() {
+    this.setState({ editing: true });
+  },
 
-    update: function() {
-        alert("whoa");
-    },
+  update: function() {
+    alert("whoa");
+  },
 
-    render: function() {
-        var classNames = ["todo-item"];
+  render: function() {
+    var classNames = ["todo-item"];
 
-        if (this.state.completed) {
-          classNames.push("completed");
-        }
-
-        if (this.state.editing) {
-          classNames.push("editing");
-        }
-
-        return (
-            React.createElement("div", {className:  classNames.join(" ") }, 
-                React.createElement("p", null, 
-                    React.createElement("input", {type: "checkbox", checked:  this.state.completed, onChange:  this.toggleCompleted}), 
-                    React.createElement("label", {onDoubleClick:  this.toggleEditing},  this.props.title), 
-                    React.createElement("input", {value:  this.props.title, className: "editor"})
-                )
-            )
-        );
+    if (this.state.completed) {
+      classNames.push("completed");
     }
+
+    if (this.state.editing) {
+      classNames.push("editing");
+    }
+
+    return (
+      React.createElement("div", {className:  classNames.join(" ") }, 
+        React.createElement("p", null, 
+          React.createElement("input", {type: "checkbox", checked:  this.state.completed, onChange:  this.toggleCompleted}), 
+          React.createElement("label", {onDoubleClick:  this.toggleEditing},  this.props.title), 
+          React.createElement("input", {value:  this.props.title, className: "editor"})
+        )
+      )
+    );
+  }
 });
 
 var TodoList = React.createClass({displayName: "TodoList",
-    getDefaultProps: function() {
-        return {
-            items: []
-        };
-    },
+  getDefaultProps: function() {
+    return {
+      items: []
+    };
+  },
 
-    render: function() {
-        var items = this.props.items.map(function(i) {
-            return React.createElement(TodoItem, {key:  i.title, title:  i.title, completed:  i.completed});
-        });
+  render: function() {
+    var items = this.props.items.map(function(i) {
+      return React.createElement(TodoItem, {key:  i.title, title:  i.title, completed:  i.completed});
+    });
 
-        return (
-            React.createElement("div", null, 
-                React.createElement("h1", null, "Todo"), 
-                 items 
-            )
-        );
-    }
+    return (
+      React.createElement("div", null, 
+        React.createElement("h1", null, "Todo"), 
+         items 
+      )
+    );
+  }
 });
 
 
--- src/index.js
+++ src/index.js
@@ -7,90 +7,90 @@
 //
 
 var items = [
-    {
-        title: "Thing #1",
-        completed: false
-    },
-    {
-        title: "Thing #2",
-        completed: true
-    },
-    {
-        title: "Thing #3",
-        completed: false
-    }
+  {
+    title: "Thing #1",
+    completed: false
+  },
+  {
+    title: "Thing #2",
+    completed: true
+  },
+  {
+    title: "Thing #3",
+    completed: false
+  }
 ];
 
 var TodoItem = React.createClass({
 
-    getDefaultProps: function() {
-        return {
-            title     : '',
-            completed : false
-        };
-    },
+  getDefaultProps: function() {
+    return {
+      title   : '',
+      completed : false
+    };
+  },
 
-    getInitialState: function() {
-        return {
-            completed : this.props.completed,
-            editing   : false
-        };
-    },
+  getInitialState: function() {
+    return {
+      completed : this.props.completed,
+      editing   : false
+    };
+  },
 
-    toggleCompleted: function(v) {
-        this.setState({ completed: !this.state.completed });
-    },
+  toggleCompleted: function(v) {
+    this.setState({ completed: !this.state.completed });
+  },
 
-    toggleEditing: function() {
-        this.setState({ editing: true });
-    },
+  toggleEditing: function() {
+    this.setState({ editing: true });
+  },
 
-    update: function() {
-        alert("whoa");
-    },
+  update: function() {
+    alert("whoa");
+  },
 
-    render: function() {
-        var classNames = ["todo-item"];
+  render: function() {
+    var classNames = ["todo-item"];
 
-        if (this.state.completed) {
-          classNames.push("completed");
-        }
-
-        if (this.state.editing) {
-          classNames.push("editing");
-        }
-
-        return (
-            <div className={ classNames.join(" ") }>
-                <p>
-                    <input type="checkbox" checked={ this.state.completed } onChange={ this.toggleCompleted } />
-                    <label onDoubleClick={ this.toggleEditing }>{ this.props.title }</label>
-                    <input value={ this.props.title } className="editor" />
-                </p>
-            </div>
-        );
+    if (this.state.completed) {
+      classNames.push("completed");
     }
+
+    if (this.state.editing) {
+      classNames.push("editing");
+    }
+
+    return (
+      <div className={ classNames.join(" ") }>
+        <p>
+          <input type="checkbox" checked={ this.state.completed } onChange={ this.toggleCompleted } />
+          <label onDoubleClick={ this.toggleEditing }>{ this.props.title }</label>
+          <input value={ this.props.title } className="editor" />
+        </p>
+      </div>
+    );
+  }
 });
 
 var TodoList = React.createClass({
-    getDefaultProps: function() {
-        return {
-            items: []
-        };
-    },
+  getDefaultProps: function() {
+    return {
+      items: []
+    };
+  },
 
-    render: function() {
-        var items = this.props.items.map(function(i) {
-            return <TodoItem key={ i.title } title={ i.title } completed={ i.completed } />;
-        });
+  render: function() {
+    var items = this.props.items.map(function(i) {
+      return <TodoItem key={ i.title } title={ i.title } completed={ i.completed } />;
+    });
 
-        return (
-            <div>
-                <h1>Todo</h1>
-                { items }
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>Todo</h1>
+        { items }
+      </div>
+    );
+  }
 });
 
 
